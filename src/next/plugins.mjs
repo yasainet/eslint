e67655c783096f5ea4515c3a9e0071a7b89419fe
yasainet <mail@yasainet.com>
2026-02-28 06:09:47 +0900

    update

--- src/next/plugins.mjs
+++
@@ -1,7 +0,0 @@
-/**
- * @fileoverview Next.js-specific ESLint plugin imports.
- */
-
-import reactYouMightNotNeedAnEffect from "eslint-plugin-react-you-might-not-need-an-effect";
-
-export { reactYouMightNotNeedAnEffect };
