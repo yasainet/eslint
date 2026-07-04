d56700a6f99761aaa433336543e523ba0d70b8c8
yasainet <mail@yasainet.com>
2026-07-04 11:48:40 +0900

    refactor(common): drop unused plugins named export
    _internal/plugins.mjs の plugins オブジェクトは誰も import していない
    (consumer 側は checkFile / stylistic / simpleImportSortPlugin を named import)。
    dead code として削除。
    
    refs #2 (item 4)

--- src/common/_internal/plugins.mjs
+++ src/common/_internal/plugins.mjs
@@ -2,10 +2,4 @@ import stylistic from "@stylistic/eslint-plugin";
 import checkFile from "eslint-plugin-check-file";
 import simpleImportSortPlugin from "eslint-plugin-simple-import-sort";
 
-export const plugins = {
-  "@stylistic": stylistic,
-  "check-file": checkFile,
-  "simple-import-sort": simpleImportSortPlugin,
-};
-
 export { checkFile, simpleImportSortPlugin, stylistic };
